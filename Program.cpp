3af4385b38f0c2bbe94f31e23427b9bd46806028
margaXD <58165730+margaXD@users.noreply.github.com>
2020-10-07 19:21:49 +0300

    Add files via upload

---
+++ Program.cpp
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hashai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
