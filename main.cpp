a607c39f5480b4c68c86deef5dedf8bf125aa4da
margaXD <58165730+margaXD@users.noreply.github.com>
2020-10-15 21:57:35 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@ int main()
 	int NumberOfLetterA = 1; // Number of letter a in document + 1
 	int NumberOfLetterE = 1; // Number of number 1 in document + 1
 	std::string pasirinkimas;
+	time_t start, end;
 	int value;
 	hashas hashasX;
-	std::ifstream fd("random file.txt");
+	std::ifstream fd("konstitucija.txt");
 	std::string raktas;
 	std::cout << "Hashinsim faila ar inputa?" << std::endl;
 	std::cin >> pasirinkimas;
@@ -38,7 +39,11 @@ int main()
 		std::cout << "NumberOfLetterE = " << NumberOfLetterE << std::endl;
 		std::cout << std::endl;
 		std::cout << "UniqueHashSeed = " << UniqueHashSeed << std::endl;
+		time(&start);
 		std::string ats = hashasX.Hashavimas(UniqueHashSeed, skaitliukasSPACE, maxValue, charValue, NumberOfLetterA, NumberOfLetterE);
+		time(&end);
+		double time_taken = double(end - start);
+		std::cout << "HASHAVIMAS UZTRUKO : " << time_taken << std::setprecision(4) << std::endl;
 		std::cout << "Hash = " << ats << std::endl;
 
 
@@ -57,7 +62,11 @@ int main()
 		std::cout << "NumberOfLetterE = " << NumberOfLetterE << std::endl;
 		std::cout << std::endl;
 		std::cout << "UniqueHashSeed = " << UniqueHashSeed << std::endl;
+		time(&start);
 		std::string ats = hashasX.Hashavimas2(UniqueHashSeed, charValue, NumberOfLetterA, NumberOfLetterE);
+		time(&end);
+		double time_taken = double(end - start);
+		std::cout << "HASHAVIMAS UZTRUKO : " <<  time_taken << std::setprecision(4) << std::endl;
 		std::cout << "Hash = " << ats << std::endl;
 	}
 }
